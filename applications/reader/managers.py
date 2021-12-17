a4a8ac442a84c13de5971b1ac45cba9c5771e2ce
eliashhtores <eliashhtorres@gmail.com>
2021-12-17 17:35:15 -0600

    Additional functions inside values

--- applications/reader/managers.py
+++ applications/reader/managers.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Avg, Sum, Count
+from django.db.models.functions import Lower
 
 
 class BookLoanManager(models.Manager):
@@ -7,7 +8,8 @@ class BookLoanManager(models.Manager):
         return self.filter(book_id=5).aggregate(Avg('reader__age'), Sum('reader__age'))
 
     def num_book_loans(self):
-        results = self.values('book').annotate(loans=Count('book'))
+        results = self.values('book').annotate(
+            loans=Count('book'), title=Lower('book__title'))
         for result in results:
             print(result, result['loans'])
         return results
